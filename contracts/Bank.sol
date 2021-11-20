5e712bbff304fae785b982dd75cd5b965b51b449
Razvan Radulescu <43811028+h3xxit@users.noreply.github.com>
2021-11-20 16:03:26 +0100

    No imports

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,10 +1,292 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.7.0;
 
-import "./interfaces/IBank.sol";
-import "./interfaces/IPriceOracle.sol";
-import "./libraries/Math.sol";
-import "@openzeppelin/contracts@3.4.2-solc-0.7/token/ERC20/ERC20.sol";
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+library DSMath {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, "ds-math-add-overflow");
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, "ds-math-sub-underflow");
+    }
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+    }
+
+    function min(uint x, uint y) internal pure returns (uint z) {
+        return x <= y ? x : y;
+    }
+    function max(uint x, uint y) internal pure returns (uint z) {
+        return x >= y ? x : y;
+    }
+    function imin(int x, int y) internal pure returns (int z) {
+        return x <= y ? x : y;
+    }
+    function imax(int x, int y) internal pure returns (int z) {
+        return x >= y ? x : y;
+    }
+
+    uint constant WAD = 10 ** 18;
+    uint constant RAY = 10 ** 27;
+
+    //rounds to zero if x*y < WAD / 2
+    function wmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), WAD / 2) / WAD;
+    }
+    //rounds to zero if x*y < WAD / 2
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), RAY / 2) / RAY;
+    }
+    //rounds to zero if x*y < WAD / 2
+    function wdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, WAD), y / 2) / y;
+    }
+    //rounds to zero if x*y < RAY / 2
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, RAY), y / 2) / y;
+    }
+
+    // This famous algorithm is called "exponentiation by squaring"
+    // and calculates x^n with x as fixed-point and n as regular unsigned.
+    //
+    // It's O(log n), instead of O(n) for naive repeated multiplication.
+    //
+    // These facts are why it works:
+    //
+    //  If n is even, then x^n = (x^2)^(n/2).
+    //  If n is odd,  then x^n = x * x^(n-1),
+    //   and applying the equation for even x gives
+    //    x^n = x * (x^2)^((n-1) / 2).
+    //
+    //  Also, EVM division is flooring and
+    //    floor[(n-1) / 2] = floor[n / 2].
+    //
+    function rpow(uint x, uint n) internal pure returns (uint z) {
+        z = n % 2 != 0 ? x : RAY;
+
+        for (n /= 2; n != 0; n /= 2) {
+            x = rmul(x, x);
+
+            if (n % 2 != 0) {
+                z = rmul(z, x);
+            }
+        }
+    }
+}
+
+
+interface IPriceOracle {
+    /**
+     * The purpose of this function is to retrieve the price of the given token
+     * in ETH. For example if the price of a HAK token is worth 0.5 ETH, then
+     * this function will return 500000000000000000 (5e17) because ETH has 18 
+     * decimals. Note that this price is not fixed and might change at any moment,
+     * according to the demand and supply on the open market.
+     * @param token - the ERC20 token for which you want to get the price in ETH.
+     * @return - the price in ETH of the given token at that moment in time.
+     */
+    function getVirtualPrice(address token) view external returns (uint256);
+}
+
+
+interface IBank {
+    struct Account { // Note that token values have an 18 decimal precision
+        uint256 deposit;           // accumulated deposits made into the account
+        uint256 interest;          // accumulated interest
+        uint256 lastInterestBlock; // block at which interest was last computed
+    }
+    // Event emitted when a user makes a deposit
+    event Deposit(
+        address indexed _from, // account of user who deposited
+        address indexed token, // token that was deposited
+        uint256 amount // amount of token that was deposited
+    );
+    // Event emitted when a user makes a withdrawal
+    event Withdraw(
+        address indexed _from, // account of user who withdrew funds
+        address indexed token, // token that was withdrawn
+        uint256 amount // amount of token that was withdrawn
+    );
+    // Event emitted when a user borrows funds
+    event Borrow(
+        address indexed _from, // account who borrowed the funds
+        address indexed token, // token that was borrowed
+        uint256 amount, // amount of token that was borrowed
+        uint256 newCollateralRatio // collateral ratio for the account, after the borrow
+    );
+    // Event emitted when a user (partially) repays a loan
+    event Repay(
+        address indexed _from, // accout which repaid the loan
+        address indexed token, // token that was borrowed and repaid
+        uint256 remainingDebt // amount that still remains to be paid (including interest)
+    );
+    // Event emitted when a loan is liquidated
+    event Liquidate(
+        address indexed liquidator, // account which performs the liquidation
+        address indexed accountLiquidated, // account which is liquidated
+        address indexed collateralToken, // token which was used as collateral
+                                         // for the loan (not the token borrowed)
+        uint256 amountOfCollateral, // amount of collateral token which is sent to the liquidator
+        uint256 amountSentBack // amount of borrowed token that is sent back to the
+                               // liquidator in case the amount that the liquidator
+                               // sent for liquidation was higher than the debt of the liquidated account
+    );
+    /**
+     * The purpose of this function is to allow end-users to deposit a given 
+     * token amount into their bank account.
+     * @param token - the address of the token to deposit. If this address is
+     *                set to 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE then 
+     *                the token to deposit is ETH.
+     * @param amount - the amount of the given token to deposit.
+     * @return - true if the deposit was successful, otherwise revert.
+     */
+    function deposit(address token, uint256 amount) payable external returns (bool);
+
+    /**
+     * The purpose of this function is to allow end-users to withdraw a given 
+     * token amount from their bank account. Upon withdrawal, the user must
+     * automatically receive a 3% interest rate per 100 blocks on their deposit.
+     * @param token - the address of the token to withdraw. If this address is
+     *                set to 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE then 
+     *                the token to withdraw is ETH.
+     * @param amount - the amount of the given token to withdraw. If this param
+     *                 is set to 0, then the maximum amount available in the 
+     *                 caller's account should be withdrawn.
+     * @return - the amount that was withdrawn plus interest upon success, 
+     *           otherwise revert.
+     */
+    function withdraw(address token, uint256 amount) external returns (uint256);
+      
+    /**
+     * The purpose of this function is to allow users to borrow funds by using their 
+     * deposited funds as collateral. The minimum ratio of deposited funds over 
+     * borrowed funds must not be less than 150%.
+     * @param token - the address of the token to borrow. This address must be
+     *                set to 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE, otherwise  
+     *                the transaction must revert.
+     * @param amount - the amount to borrow. If this amount is set to zero (0),
+     *                 then the amount borrowed should be the maximum allowed, 
+     *                 while respecting the collateral ratio of 150%.
+     * @return - the current collateral ratio.
+     */
+    function borrow(address token, uint256 amount) external returns (uint256);
+     
+    /**
+     * The purpose of this function is to allow users to repay their loans.
+     * Loans can be repaid partially or entirely. When replaying a loan, an
+     * interest payment is also required. The interest on a loan is equal to
+     * 5% of the amount lent per 100 blocks. If the loan is repaid earlier,
+     * or later then the interest should be proportional to the number of 
+     * blocks that the amount was borrowed for.
+     * @param token - the address of the token to repay. If this address is
+     *                set to 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE then 
+     *                the token is ETH.
+     * @param amount - the amount to repay including the interest.
+     * @return - the amount still left to pay for this loan, excluding interest.
+     */
+    function repay(address token, uint256 amount) payable external returns (uint256);
+     
+    /**
+     * The purpose of this function is to allow so called keepers to collect bad
+     * debt, that is in case the collateral ratio goes below 150% for any loan. 
+     * @param token - the address of the token used as collateral for the loan. 
+     * @param account - the account that took out the loan that is now undercollateralized.
+     * @return - true if the liquidation was successful, otherwise revert.
+     */
+    function liquidate(address token, address account) payable external returns (bool);
+ 
+    /**
+     * The purpose of this function is to return the collateral ratio for any account.
+     * The collateral ratio is computed as the value deposited divided by the value
+     * borrowed. However, if no value is borrowed then the function should return 
+     * uint256 MAX_INT = type(uint256).max
+     * @param token - the address of the deposited token used a collateral for the loan. 
+     * @param account - the account that took out the loan.
+     * @return - the value of the collateral ratio with 2 percentage decimals, e.g. 1% = 100.
+     *           If the account has no deposits for the given token then return zero (0).
+     *           If the account has deposited token, but has not borrowed anything then 
+     *           return MAX_INT.
+     */
+    function getCollateralRatio(address token, address account) view external returns (uint256);
+
+    /**
+     * The purpose of this function is to return the balance that the caller 
+     * has in their own account for the given token (including interest).
+     * @param token - the address of the token for which the balance is computed.
+     * @return - the value of the caller's balance with interest, excluding debts.
+     */
+    function getBalance(address token) view external returns (uint256);
+}
 
 contract Bank is IBank {
     address internal constant ethToken = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
